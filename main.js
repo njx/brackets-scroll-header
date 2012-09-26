124b044c2afca0a9b5b8119c9035dad0d6c50d0a
Narciso Jaramillo <njaramil@adobe.com>
2012-09-26 11:34:24 -0700

    Initial working implementation. Still too slow on change events to be useful.

--- main.js
+++ main.js
@@ -1,32 +1,89 @@
 /*jslint vars: true, plusplus: true, devel: true, nomen: true, indent: 4, maxerr: 50 */
 /*global define, brackets, $, CodeMirror */
 
+// BUGS:
+// * WAY too slow on change events. Re-searches for all functions on each change.
+// * In many files, horizontal position of the header text doesn't match its horizontal position in the document
+//   (probably because of max line number width difference).
+// * Function name isn't color-coded the same way as in the original document.
+// * Horizontal position doesn't track document scroll position.
+// * Line number in header isn't visible (it's pushed to the bottom of the header area).
+
+// TODO:
+// * Make it work for CSS media queries.
+
 define(function (require, exports, module) {
     "use strict";
     
     var EditorManager = brackets.getModule("editor/EditorManager"),
-        ExtensionUtils = brackets.getModule("utils/ExtensionUtils");
+        ExtensionUtils = brackets.getModule("utils/ExtensionUtils"),
+        JSUtils = brackets.getModule("language/JSUtils");
     
-    var $header, headerCM, editor, ranges = [], visible = false;
+    var $header, headerCM, editor, ranges = [], curRange, visible = false;
     
     function update() {
-        // find the topmost visible line
-        // if header is visible
-        //      if topmost visible line is the end of the current range or not in a range, scroll header out
-        //      if topmost visible line is in a range that's not the header's range, update header (with anim?)
-        // if header is invisible 
-        //      if topmost visible line is a header or in a range, show the header at position 0 and update its text
+        // find the topmost visible line and which range it's in
+        var editorTop = $(editor._codeMirror.getWrapperElement()).offset().top,
+            topLine = editor._codeMirror.coordsChar({x: 0, y: editorTop}).line,
+            topLineY = editor._codeMirror.charCoords({line: topLine, ch: 0}).y,
+            i,
+            coords,
+            newRange;
+
+        // Even if the current top line is inside the current range, we still need to re-search
+        // because it might be in a nested range.
+        for (i = 0; i < ranges.length; i++) {
+            if (topLine < ranges[i].lineStart) {
+                break;
+            } else if (topLine >= ranges[i].lineStart && topLine <= ranges[i].lineEnd) {
+                newRange = ranges[i];
+                // Don't break yet because we want to find the narrowest range.
+            }
+        }
         
-        // TODO: should header be opaque?
+        if (!newRange) {
+            curRange = null;
+            if (visible) {
+                hide();
+            }
+            return;
+        }
         
-        // ***
+        if (newRange != curRange) {
+            curRange = newRange;
+            headerCM.setOption("mode", editor._codeMirror.getOption("mode"));
+            headerCM.setOption("theme", editor._codeMirror.getOption("theme"));
+            headerCM.setOption("lineNumbers", editor._codeMirror.getOption("lineNumbers"));
+            headerCM.setOption("firstLineNumber", topLine + 1);
+            headerCM.setValue(editor._codeMirror.getLine(curRange.lineStart));
+
+            coords = editor._codeMirror.charCoords({line: 0, ch: 0});
+            var height = coords.yBot - coords.y + 4;
+            $header.height(height);
+            $(headerCM.getScrollerElement()).height(height);
+        }
+        
+        if (topLine === curRange.lineEnd) {
+            $header.css("top", topLineY - editorTop);
+        } else {
+            $header.css("top", 0);
+        }
+        
+        if (!visible) {
+            show();
+            headerCM.refresh();
+        }
     }
     
     function handleDocumentChange() {
         // update ranges based on change (brute force scan every time for now)
+        // TODO: incrementally update ranges. This is way too slow.
         // TODO: have different providers for different file types
-        // ***
-        
+        ranges = JSUtils.findAllMatchingFunctionsInText(editor.document.getText(), "*")
+            .sort(function (a, b) {
+                return a.lineStart - b.lineStart;
+            });
+        curRange = null;        
         update();
     }
     
@@ -41,25 +98,40 @@ define(function (require, exports, module) {
     }
     
     function setEditor() {
+        // We only deal with the outer editor (not inline editors).
+        var curEditor = EditorManager.getCurrentFullEditor();
+        if (curEditor === editor) {
+            return;
+        }
+        
         if (editor) {
-            $(editor).off("scroll", handleScroll);
+            $(editor).off("scroll", update);
             $(editor.document).off("change", handleDocumentChange);
         }
         hide();
-        $header.remove();
-        editor = EditorManager.getFocusedEditor();
+        ranges = [];
+        curRange = null;
+        editor = curEditor;
         if (editor) {
-            $header.appendTo(editor.getRootElement());
-            $(editor).on("scroll", handleScroll);
-            headerCM.setText(editor.document.getText());
+            $(editor).on("scroll", update);
             $(editor.document).on("change", handleDocumentChange);
+            handleDocumentChange();
+            update();
         }
     }
     
     // Initialize extension
     ExtensionUtils.loadStyleSheet(module, "styles.css");
-    $header = $("<div id='header'></div>");
+    $header = $("<div id='brackets-scroll-header'></div>")
+        .appendTo($("#editor-holder"))
+        .hide();
     headerCM = new CodeMirror($header.get(0));
+    $(".CodeMirror-scroll", $header).css("background", "transparent");
+    $(".CodeMirror-lines", $header).css({
+        paddingTop: 2,
+        paddingBottom: 2
+    });
     
     $(EditorManager).on("focusedEditorChange", setEditor);
+    setEditor();
 });
